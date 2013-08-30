030faef0fa18394e1608df2ba8a75a6e34d8d8bd
Manohar Prabhu <manoharprabhu@manoharprabhu-AO725.(none)>
2013-08-31 04:25:56 +0530

    Generate mines at random. Mark the squares with mines with a white B

--- Main.js
+++ Main.js
@@ -1,3 +1,20 @@
+var minesArray;
+
+function Create2DArray(rows) {
+  var arr = [];
+
+  for (var i=0;i<rows;i++) {
+     arr[i] = [];
+  }
+
+  return arr;
+}
+
+
+
+
+
+
 function drawGrid(){
 	 var htmlString = "<table>";
      var row,col;
@@ -8,12 +25,26 @@ function drawGrid(){
   	   htmlString += "<tr>";
   	   for(col=1;col<=10;col++){
   		   
-  		 htmlString += "<td id=\"button_"+row+"_"+col+"\"><input type=\"button\" class=\"square_cells\"/></td>";  
+  		 htmlString += "<td><button id=\"button_"+row+"_"+col+"\" class=\"square_cells\"/></td>";  
   	   }
   	   htmlString += "</tr>";
      }
     
      htmlString += "</table>";
     $('#mainGrid').html(htmlString);
- 
+    
+    minesArray = Create2DArray(20);
+    
+    for(row=1;row<=10;row++){
+    	for(col=1;col<=10;col++){
+    		if(Math.floor(Math.random()*2) == 0 ) {
+    		minesArray[row][col] = true;
+    		$('#button_'+row+'_'+col).html("B");
+    		}
+    		else {
+    		minesArray[row][col] = false;
+    		}
+    	}
+    }
+   
 }
