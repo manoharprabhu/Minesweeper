377855694fb9360a36b09fcf33934321bd4212d1
Manohar Prabhu <manoharprabhu@manoharprabhu-AO725.(none)>
2013-09-02 03:16:04 +0530

    Changed clicked cell colors to green. refined mining process. Have to cover corner cases. Added a dummy row and column to define the border cells.

--- Main.js
+++ Main.js
@@ -16,32 +16,26 @@ function squareClick(row,col){
 	if(parseInt(row) < 1 || parseInt(col) < 1 || parseInt(row) > 10 || parseInt(col) > 10)
 	return;
 	//alert($('#button_'+row+'_'+col).css("background-color"));
-	if(document.getElementById('button_'+row+'_'+col).style.backgroundColor == "red" )
+	if(document.getElementById('button_'+row+'_'+col).style.backgroundColor == "green" )
 	return;
 	
 	
-	if($('#button_'+row+'_'+col).html() == "B")
+	if(minesArray[row][col] == true )
 	{
+		gameOver();
 		return;
 	} else {
-		if($('#button_'+parseInt(row-1)+'_'+parseInt(col-1)).html() != "B" &&
-			$('#button_'+parseInt(row-1)+'_'+parseInt(col)).html() != "B" &&
-			$('#button_'+parseInt(row-1)+'_'+parseInt(col+1)).html() != "B" &&
-			$('#button_'+parseInt(row)+'_'+parseInt(col-1)).html() != "B" &&
-			$('#button_'+parseInt(row)+'_'+parseInt(col+1)).html() != "B" &&
-			$('#button_'+parseInt(row+1)+'_'+parseInt(col-1)).html() != "B" &&
-			$('#button_'+parseInt(row+1)+'_'+parseInt(col)).html() != "B" &&
-			$('#button_'+parseInt(row+1) +'_'+ parseInt(col+1)).html() != "B") {
+		if(minesArray[row-1][col-1] == false &&
+			minesArray[row-1][col] == false  &&
+			minesArray[row-1][col+1] == false  &&
+			minesArray[row][col-1] == false  &&
+			minesArray[row][col+1] == false  &&
+			minesArray[row+1][col-1] == false  &&
+			minesArray[row+1][col] == false  &&
+			minesArray[row+1][col+1] == false ) {
 				
-			$('#button_'+row+'_'+col).css("background","red");
-			//$('#button_'+parseInt(row-1)+'_'+parseInt(col-1)).css("background","red");
-			//$('#button_'+parseInt(row-1)+'_'+parseInt(col)).css("background","red");
-			//$('#button_'+parseInt(row-1)+'_'+parseInt(col+1)).css("background","red");
-			//$('#button_'+parseInt(row)+'_'+parseInt(col-1)).css("background","red");
-			//$('#button_'+parseInt(row)+'_'+parseInt(col+1)).css("background","red");
-			//$('#button_'+parseInt(row+1)+'_'+parseInt(col-1)).css("background","red");
-			//$('#button_'+parseInt(row+1)+'_'+parseInt(col)).css("background","red");
-			//$('#button_'+parseInt(row+1) +'_'+ parseInt(col+1)).css("background","red");
+			$('#button_'+row+'_'+col).css("background","green");
+			
 				
 			squareClick(row-1,col-1);
 			squareClick(row-1,col);
@@ -54,30 +48,77 @@ function squareClick(row,col){
 				
 			
 			} else {
-				$('#button_'+row+'_'+col).css("background","red");
-				return;
+				$('#button_'+row+'_'+col).css("background","green");
 			}
 		
 	}
 }
 
+function getCountOfAdjacentMines(i,j){
+	var count=0;
+	if(minesArray[i-1][j-1] == true)
+		count++;
+	if(minesArray[i-1][j] == true)
+		count++;
+	if(minesArray[i-1][j+1] == true)
+		count++;
+	if(minesArray[i][j-1] == true)
+		count++;
+	if(minesArray[i][j+1] == true)
+		count++;
+	if(minesArray[i+1][j-1] == true)
+		count++;
+	if(minesArray[i+1][j] == true)
+		count++;
+	if(minesArray[i+1][j+1] == true)
+		count++;
+		
+		if(count!=0)
+			return count;
+		else
+			return "";
+		
+		
+}
+
 function initiateSquareClick(row,col)
 {
-	reccursionCount = 0;
+	//reccursionCount = 0;
 	squareClick(row,col);
-	alert(reccursionCount);
+	//alert(reccursionCount);
+	var i,j;
+	for(i=1;i<=10;i++){
+		for(j=1;j<=10;j++){
+			if(document.getElementById('button_'+i+'_'+j).style.backgroundColor == "green" ){
+				$('#button_'+i+'_'+j).html(getCountOfAdjacentMines(i,j));
+				$('#button_'+i+'_'+j).css("color","white");
+				//$('#button_'+i+'_'+j).attr("disabled",true);
+				
+				
+				$('#button_'+row+'_'+col).css("background","green");
+			}
+		}
+	}
 }
+
+function gameOver(){
+	alert("gameover");
+}
+
 function drawGrid(){
 	 var htmlString = "<table>";
      var row,col;
      row=0;
      col=0;
      
-     for(row=1;row<=10;row++) {
+     for(row=0;row<=11;row++) {
   	   htmlString += "<tr>";
-  	   for(col=1;col<=10;col++){
-  		   
-  		 htmlString += "<td><button id=\"button_"+row+"_"+col+"\" class=\"square_cells\"/ onclick=\"initiateSquareClick("+row+","+col+");\"></td>";  
+  	   for(col=0;col<=11;col++){
+  		   if(row==0 || row == 11 || col == 0 || col == 11) {
+  		   	htmlString += "<td><button id=\"button_"+row+"_"+col+"\" style=\"background-color:green;display:none;\" \></td>";
+  		   } else {
+  		 htmlString += "<td><button id=\"button_"+row+"_"+col+"\" class=\"square_cells\"/ onclick=\"initiateSquareClick("+row+","+col+");\"></td>";
+  		 }  
   	   }
   	   htmlString += "</tr>";
      }
@@ -91,7 +132,6 @@ function drawGrid(){
     	for(col=1;col<=10;col++){
     		if(Math.floor(Math.random()*10) == 0 ) {
     		minesArray[row][col] = true;
-    		$('#button_'+row+'_'+col).html("B");
     		}
     		else {
     		minesArray[row][col] = false;
