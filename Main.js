7afd228f8ba02e11d3e0932bdaa5bc40302c7679
manoharprabhu <manoharagent13@gmail.com>
2013-09-02 19:15:04 +0530

    Added comments and cleaned up the code.

--- Main.js
+++ Main.js
@@ -12,18 +12,22 @@ function Create2DArray(rows) {
 
 function squareClick(row,col){
 	
+	//Prevent reccursion from running off the grid bounds
 	if(parseInt(row) < 0 || parseInt(col) < 0 || parseInt(row) > 11 || parseInt(col) > 11)
 	return;
-
+	
+	//Do not explore the grid if the current cell has already been explored
 	if(document.getElementById('button_'+row+'_'+col).style.backgroundColor == "green" )
 	return;
 	
 	
 	if(minesArray[row][col] == true )
 	{
+		//If the current cell contains mine, game over ;)
 		gameOver();
 		return;
 	} else {
+		//If all the neighbours of clicked cell doesnt contain any mines, explore the adjacent cells reccursively.
 		$('#button_'+row+'_'+col).css("background","green");
 		if(minesArray[row-1][col-1] == false &&
 			minesArray[row-1][col] == false  &&
@@ -51,6 +55,7 @@ function squareClick(row,col){
 	}
 }
 
+//Get the number of mines adjacent to the given cell
 function getCountOfAdjacentMines(i,j){
 	var count=0;
 	if(minesArray[i-1][j-1] == true)
@@ -80,18 +85,15 @@ function getCountOfAdjacentMines(i,j){
 
 function initiateSquareClick(row,col)
 {
-	//reccursionCount = 0;
 	squareClick(row,col);
-	//alert(reccursionCount);
 	var i,j;
+	
+	//Update the mine count that shows up on the uncovered cells.
 	for(i=1;i<=10;i++){
 		for(j=1;j<=10;j++){
 			if(document.getElementById('button_'+i+'_'+j).style.backgroundColor == "green" ){
 				$('#button_'+i+'_'+j).html(getCountOfAdjacentMines(i,j));
-				$('#button_'+i+'_'+j).css("color","white");
-				//$('#button_'+i+'_'+j).attr("disabled",true);
-				
-				
+				$('#button_'+i+'_'+j).css("color","white");	
 				$('#button_'+row+'_'+col).css("background","green");
 			}
 		}
@@ -108,6 +110,7 @@ function drawGrid(){
      row=0;
      col=0;
      
+	 //Draw the grid on the page. Also, have a extra row and column at beggining and end of the grid to define the boundary
      for(row=0;row<=11;row++) {
   	   htmlString += "<tr>";
   	   for(col=0;col<=11;col++){
@@ -125,6 +128,7 @@ function drawGrid(){
     
     minesArray = Create2DArray(20);
     
+	//Generate a mine field.
     for(row=0;row<=11;row++){
     	for(col=0;col<=11;col++){
 		
